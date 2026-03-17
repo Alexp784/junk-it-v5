9393d240acaac81327f67afbf071b0d514a8e619
AlexanderMcMarshall <103285506+AlexanderMcMarshall@users.noreply.github.com>
2026-03-17 14:19:20 +0000

    Dev1

--- vite.config.ts
+++ vite.config.ts
@@ -13,6 +13,7 @@ export default defineConfig(({ mode }) => ({
     },
   },
   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
+  base: "/junk-it-blast/",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
